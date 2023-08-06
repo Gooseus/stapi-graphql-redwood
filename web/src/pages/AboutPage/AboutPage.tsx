04d46530646c1a9707f2e3d2127cb8d90bc5281a
Shawn Marincas <shawngmarincas@gmail.com>
2023-08-05 20:37:47 -0400

    fix: About page

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -6,11 +6,7 @@ const AboutPage = () => {
       <MetaTags title="About" description="About page" />
       <h3>About</h3>
 
-      <p>
-        This was built as an exercise to see what I could do with RedwoodJS in a
-        short amount of time that wasn&rsquo;t just a Blog tutorial. I literally
-        just built a blog last week.
-      </p>
+      <p>Built by Shawn Marincas for exploration and educational purposes.</p>
     </>
   )
 }
